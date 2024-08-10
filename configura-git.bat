a39fa6ee5a2445e887af530f4ef2b679fb1f2515
Winnye Souza <wiinnye@gmail.com>
2024-08-09 21:41:59 -0300

    Primeira Pagina Web

---
+++ configura-git.bat
@@ -0,0 +1,14 @@
+echo ON
+
+echo --- Configurando Usuario GitHUB ----
+echo
+echo
+git config --global user.name "Winnye Souza"
+git config --global user.email "wiinnye@gmail.com"
+echo --- Mostrando a configuração atual do GitHub no computador
+git config --list
+echo
+echo --- Fim ---
+echo
+
+pause
